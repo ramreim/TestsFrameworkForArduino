808e453ba96cd1af2ff2b9344f92916d338db139
remreim <studen_studen@yahoo.com>
2024-07-28 12:33:49 +0300

    libs added

---
+++ lib/ExecuteCmd.h
@@ -0,0 +1,55 @@
+#ifndef EXECUTE_CMD_H
+#define EXECUTE_CMD_H
+
+#include "Result.h"
+
+Result result;
+
+Result DigitalReadCmd(int pin)
+{
+	result.pinNumber = pin;
+	result.functionNr = 1;
+	//result.functionResult = pin + result.functionNr;
+#ifndef ArduinoTestFramework
+	result.functionResult = digitalRead(pin);
+#endif
+	return result;
+}
+
+Result DigitalWriteCmd(int pin, int val)
+{
+	result.pinNumber = pin;
+	result.functionNr = 2;
+	result.functionResult = pin + result.functionNr;
+#ifndef ArduinoTestFramework
+	digitalWrite(pin, val);
+#endif	
+	result.functionResult = val;
+	return result;
+}
+
+Result AnalogReadCmd(int pin)
+{
+	result.pinNumber = pin;
+	result.functionNr = 3;
+	//result.functionResult = pin + result.functionNr;	
+#ifndef ArduinoTestFramework
+	result.functionResult = analogRead(pin);
+#endif	
+	return result;
+}
+
+Result AnalogWriteCmd(int pin, int val)
+{
+	result.pinNumber = pin;
+	result.functionNr = 4;
+	result.functionResult = pin + result.functionNr;	
+#ifndef ArduinoTestFramework
+	analogWrite(pin, val);
+#endif	
+	result.functionResult = val;
+	return result;
+}
+
+#endif
+// endif EXECUTE_CMD_H
