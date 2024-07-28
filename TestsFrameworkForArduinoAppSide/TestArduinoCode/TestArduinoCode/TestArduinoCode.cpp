ca21be4f65275515325a46f5781f8932aae3ff18
remreim <studen_studen@yahoo.com>
2024-07-28 12:19:45 +0300

    app side init

---
+++ TestsFrameworkForArduinoAppSide/TestArduinoCode/TestArduinoCode/TestArduinoCode.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestArduinoCode
+{
+	TEST_CLASS(TestArduinoCode)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
