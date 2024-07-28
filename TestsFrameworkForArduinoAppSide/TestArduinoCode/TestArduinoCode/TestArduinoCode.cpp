00d9dbb38c0fcec33cdfca34c1e36bd5ea6ac4d9
remreim <studen_studen@yahoo.com>
2024-07-28 12:45:33 +0300

    created digital read test 001

--- TestsFrameworkForArduinoAppSide/TestArduinoCode/TestArduinoCode/TestArduinoCode.cpp
+++ TestsFrameworkForArduinoAppSide/TestArduinoCode/TestArduinoCode/TestArduinoCode.cpp
@@ -1,4 +1,14 @@
+#ifndef ArduinoTestFramework
+#define ArduinoTestFramework
+#endif
+// endif ArduinoTestFramework
+
+
 #include "CppUnitTest.h"
+
+#include "D:\pc_07_1tb_ssd\programming\Arduino\arduino_projektai_002\test_framework_00003\lib\Result.h"
+
+#include "D:\pc_07_1tb_ssd\programming\Arduino\arduino_projektai_002\test_framework_00003\lib\ExecuteCmd.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,7 +21,19 @@ namespace TestArduinoCode
 		TEST_METHOD(TestMethod1)
 		{
 
-			Assert::AreEqual(5,5);
+			Result readD00 = DigitalReadCmd(0);
+
+			Result readD01 = DigitalReadCmd(1);
+
+			Result readD02 = DigitalReadCmd(2);
+
+			Assert::AreEqual(readD00.functionNr, 1);
+
+			Assert::AreEqual(readD01.pinNumber, 1);
+
+			Assert::AreEqual(readD01.functionNr, 1);
+
+			Assert::AreEqual(readD02.functionNr, 1);
 		}
 	};
 }
